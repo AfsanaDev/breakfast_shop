1c741372acef4abc08e06bc7d779b07a777e6974
Afsna Akter <tarinakter567@gmail.com>
2024-09-26 08:50:26 +0600

    make changes on home pages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,28 +8,56 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BreakFast',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        ) ,
-        ), 
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
-          ),
-          decoration: BoxDecoration(
-            color: Color(0xff7f8f8f),
-            borderRadius: BorderRadius.circular(18)
-          ),
-        ),
-      ),
+      appBar: appBar(),
     );
     
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text('BreakFast',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold
+      ) ,
+      ), 
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: const Color(0xff7f8f8f),
+          borderRadius: BorderRadius.circular(18)
+        ),
+        child: SvgPicture.asset(
+          'assets/icons/Arrow-Left 2.svg',
+        height: 20,
+        width: 20,
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+            
+          },
+          
+          child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color(0xff7f8f8f),
+            borderRadius: BorderRadius.circular(18)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+          height: 5,
+          ),
+                  ),
+        ),
+      ],
+    );
   }
 }
